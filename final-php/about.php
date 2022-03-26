8189a0064c36fb60a291d289a24667a2f2cdfafc
Matthew Long <97557276+MachSpeedMatt@users.noreply.github.com>
2022-03-26 15:59:44 -0500

    1

--- final-php/about.php
+++
@@ -1,5 +0,0 @@
-<?php require('components/head.inc.php'); ?>
-<?php include('components/navbar.inc.php'); ?>
-<?php include('components/features.inc.php'); ?>
-<?php include('components/contact.inc.php'); ?>
-<?php require('components/footer.inc.php'); ?>
